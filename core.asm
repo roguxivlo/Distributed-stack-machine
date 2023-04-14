e7bf712ca3c10fe54d7b1597d0442b03220f40ff
Jan Roguwski <Jroguwski@gmail.com>
2023-04-14 13:31:07 +0200

    some operations implemented

--- core.asm
+++ core.asm
@@ -7,7 +7,7 @@ extern put_value
 section .text
 
 ; Makro do wypisywania, do debugowania.
-%include "macro_print.asm"
+;%include "macro_print.asm"
 
 ; Argumenty funkcji core:
 ;
@@ -17,115 +17,138 @@ section .text
 ; rsi = p (wskaźnik na zapis obliczenia)
 
 core:
+; Zapisujemy rsp w rbx, aby na koniec obliczenia zresetować stos:
+        ;print   "initial stack: ", rsp
+        push    rbx
+        mov     rbx, rsp
+
 ; Rozpoczynamy pętlę w której po kolei będziemy obsługiwać kolejne
 ; polecenia obliczenia.
 
 .main_loop:
-        movzx   edi, byte [rsi]         ; rejestr edi zawiera kolejny znak obliczenia.
+        movzx   edx, byte [rsi]         ; rejestr edx zawiera kolejny znak obliczenia.
                                         ; Przy okazji zerujemy starsze bity rejestru.
-        test    dil, dil                ; Sprawdzenie, czy edi == '\0'
-        jz      .return                 ; Skok wykona się gdy edi == '\0'
+        test    dl, dl                ; Sprawdzenie, czy edx == '\0'
+        jz      .return                 ; Skok wykona się gdy edx == '\0'
                                         ; w przeciwnym razie obsługujemy polecenie.
 
         ; Sprawdzenie, czy znak jest równy '+':
-        cmp     dil, '+'                ; Porównanie ze znakiem '+'
+        cmp     dl, '+'                ; Porównanie ze znakiem '+'
         jz      .plus                   ; Skok do etykiety wykonującej dodawanie.
 
         ; Sprawdzenie, czy znak jest równy '*':
-        cmp     dil, '*'                ; Porównanie ze znakiem '*'
+        cmp     dl, '*'                ; Porównanie ze znakiem '*'
         jz      .multiply               ; Skok do etykiety wykonującej mnożenie.
 
         ; Sprawdzenie, czy znak jest równy '-':
-        cmp     dil, '-'                        ; Porównanie ze znakiem '-'
+        cmp     dl, '-'                        ; Porównanie ze znakiem '-'
         jz      .negate                         ; Skok do etykiety wykonującej negację.
 
         ; Sprawdzenie, czy znak jest równy 'n':
-        cmp     dil, 'n'                        ; Porównanie ze znakiem 'n'
+        cmp     dl, 'n'                        ; Porównanie ze znakiem 'n'
         jz      .push_core_id                   ; Skok do etykiety wstawiającej n na stos.
 
         ; Sprawdzenie, czy znak jest równy 'B':
-        cmp     dil, 'B'                        ; Porównanie ze znakiem 'B'
+        cmp     dl, 'B'                        ; Porównanie ze znakiem 'B'
         jz      .jump_back                           ; Skok do etykiety wykonującej skok w obliczeniu.
 
         ; Sprawdzenie, czy znak jest równy 'C':
-        cmp     dil, 'C'                ; Porównanie ze znakiem 'C'
+        cmp     dl, 'C'                ; Porównanie ze znakiem 'C'
         jz      .throw_away                   ; Skok do etykiety zrzucającej element ze stosu.
 
         ; Sprawdzenie, czy znak jest równy 'D':
-        cmp     dil, 'D'                ; Porównanie ze znakiem 'D'
+        cmp     dl, 'D'                ; Porównanie ze znakiem 'D'
         jz      .duplicate                   ; Skok do etykiety duplikującej szczyt stosu.
 
         ; Sprawdzenie, czy znak jest równy 'E':
-        cmp     dil, 'E'                ; Porównanie ze znakiem 'E'
+        cmp     dl, 'E'                ; Porównanie ze znakiem 'E'
         jz      .swap                   ; Skok do etykiety zamieniającej miejscami dwa szczytowe elementy stosu.
 
         ; Sprawdzenie, czy znak jest równy 'G':
-        cmp     dil, 'G'                ; Porównanie ze znakiem 'G'
+        cmp     dl, 'G'                ; Porównanie ze znakiem 'G'
         jz      .get_value                   ; Skok do etykiety wołającej funkcję get_value.
 
         ; Sprawdzenie, czy znak jest równy 'P':
-        cmp     dil, 'P'                ; Porównanie ze znakiem 'P'
+        cmp     dl, 'P'                ; Porównanie ze znakiem 'P'
         jz      .put_value                   ; Skok do etykiety wołającej funkcję put_value.
 
         ; Sprawdzenie, czy znak jest równy 'S':
-        cmp     dil, 'S'                ; Porównanie ze znakiem 'S'
+        cmp     dl, 'S'                ; Porównanie ze znakiem 'S'
         jz      .sync                   ; Skok do etykiety synchronizującej.
 
 ; W przeciwnym wypadku mamy do czynienia z cyfrą:
 .put_digit:
         mov cl, '0'
-        sub     dil, cl                    ; Rejestr dil zawiera numeryczną wartość liczby.
-        print   "digit = ", rdi         ; debug
-        jmp .next_iter
+        sub     dl, cl                    ; Rejestr dl zawiera numeryczną wartość liczby.
+        ;print   "digit = ", rdx         ; debug
+        ;print   "before: ", rsp
+        push    rdx                     ; wstawiamy liczbę na stos.
+        jmp     .next_iter
 
 .return:
-        print "the end. ", rdi
+        pop     rax                     ; ładujemy szczyt stosu jako wynik
+        ;print   "the end. current stack after pop: ", rsp
+        mov     rsp, rbx                ; resetujemy stos do stanu sprzed wywołania funkcji.
+        ;print   "stack restored: ", rsp
+        pop     rbx
         ret
 
 .plus:
-        print "+ ", rdi
-        jmp .next_iter
+        pop     rax                     ; zdejmij pierwszą liczbę ze stosu
+        ;print   "+ ", rax
+        add     [rsp], rax              ; dodaj ją do wierzchołka stosu
+        jmp     .next_iter
 
 .multiply:
-        print "* ", rdi
-        jmp .next_iter
+        pop     rax                     ; zdejmij pierwszą liczbę ze stosu
+        ;print   "* ", rax
+        mul     qword [rsp]             ; pomnóż ją przez wierzchołek stosu
+        add     rsp, 8                  ; zdejmij ze stosu drugą liczbę
+        push    rax                     ; wstaw wynik na stos
+        jmp     .next_iter
 
 .negate:
-        print "- ", rdi
-        jmp .next_iter
+        pop     rax                     ; zdejmij liczbę ze stosu
+        ;print   "- ", rax               ; debugowanie
+        neg     rax                     ; zmień znak liczby
+        push    rax                     ; wstaw liczbę na stos
+        jmp     .next_iter
 
 .push_core_id:
-        print "push n ", rdi
-        jmp .next_iter
+        ;print   "push n ", rdx
+        push    rdi                     ; wstaw id rdzenia na stos
+        jmp     .next_iter
 
 .jump_back:
-        print "B ", rdi
-        jmp .next_iter
+        ;print "B ", rdx
+        jmp     .next_iter
 
 .throw_away:
-        print "C ", rdi
-        jmp .next_iter
+        ;print "C ", rdx
+        add     rsp, 8                  ; zdejmij ze stosu liczbę
+        jmp     .next_iter
 
 .duplicate:
-        print "D ", rdi
-        jmp .next_iter
+        ;print "D ", rdx
+        push    qword [rsp]                   ; wstaw na stos kopię szczytowego elementu
+        jmp     .next_iter
 
 .swap:
-        print "E ", rdi
-        jmp .next_iter
+        ;print "E ", rdx
+        jmp     .next_iter
 
 .get_value:
-        print "G ", rdi
-        jmp .next_iter
+        ;print "G ", rdx
+        jmp     .next_iter
 
 .put_value:
-        print "P ", rdi
-        jmp .next_iter
+        ;print "P ", rdx
+        jmp     .next_iter
 
 .sync:
-        print "S ", rdi
-        jmp .next_iter
+        ;print "S ", rdx
+        jmp     .next_iter
 
 .next_iter:
         inc rsi
-        jmp .main_loop
+        jmp     .main_loop
--- example.c
+++ example.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <pthread.h>
 #include <stddef.h>
+#include <stdio.h>
 
 // Ustalamy liczbę rdzeni.
 #define N 1
@@ -39,10 +40,17 @@ static void *core_thread(void *params) {
   while (wait == 0)
     ;
 
+  printf("n = %" PRIu64 ", result = %" PRIu64 "\n", cp->n, cp->result);
+
   cp->result = core(cp->n, cp->p);
+  // print n and result:
+  printf("n = %" PRIu64 ", result = %" PRIu64 "\n", cp->n, cp->result);
 
   return NULL;
 }
+
+// Definicje różnych testowych obliczeń:
+const char* digits_only = "01DC";
 
 int main() {
   static pthread_t tid[N];
@@ -53,12 +61,14 @@ int main() {
     computation[1] = "01234n+P56789E-+D+*G*1n-+S2ED+E1-+75+-BC";
   }
   if (N == 1) {
-    computation[0] = "01234n+P56789E-+D+*G*1n-+S2ED+E1-+75+-BC";
+    computation[0] = digits_only;
   }
   static const uint64_t result[2] = {112, 56};
 
   for (size_t n = 0; n < N; ++n) {
     params[n].n = n;
+    // print n:
+    printf("n = %" PRIu64 "\n", params[n].n);
     params[n].result = 0;
     params[n].p = computation[n];
   }
@@ -71,5 +81,10 @@ int main() {
 
   for (size_t n = 0; n < N; ++n) assert(0 == pthread_join(tid[n], NULL));
 
-  for (size_t n = 0; n < N; ++n) assert(params[n].result == result[n]);
+  // print results:
+  for (int i = 0; i < N; i++) {
+    printf("result[%d] = %" PRIu64 "\n", i, params[i].result);
+  }
+
+  // for (size_t n = 0; n < N; ++n) assert(params[n].result == result[n]);
 }
